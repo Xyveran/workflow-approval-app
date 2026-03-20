9cd7bb7ae329d7c3101cbad8afc1de6244aa67e9
Xyveran <edwynortiz@proton.me>
2026-03-20 18:28:01 -0400

    Updated Program.cs builder to include WorkflowExecutionEngine and AnalyticsService. Added OpenApiDocument to builder and defined swagger configuration.

--- backend/Api/WorkflowApproval.Api/Program.cs
+++ backend/Api/WorkflowApproval.Api/Program.cs
@@ -1,28 +1,61 @@
 using Microsoft.EntityFrameworkCore;
 using WorkflowApproval.Application.Interfaces;
 using WorkflowApproval.Application.Services;
+using WorkflowApproval.Application.Workflow;
 using WorkflowApproval.Infrastructure.Data;
 using WorkflowApproval.Api.Endpoints;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+// Infrastructure
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 builder.Services.AddScoped<IWorkflowDbContext>(provider => provider.GetRequiredService<AppDbContext>());
 
+// Application Services
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<WorkflowExecutionEngine>();
+builder.Services.AddScoped<AnalyticsService>();
 builder.Services.AddScoped<IWorkflowService, WorkflowService>();
 
-var app = builder.Build();
+// API / Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "TodoAPI";
+    config.Title = "WorkflowApprovalAPI v1";
+    config.Version = "v1";
+});
 
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+       config.DocumentTitle = "WorkflowApprovalAPI";
+       config.Path = "/swagger";
+       config.DocumentPath = "/swagger/{documentName}/swagger.json";
+       config.DocExpansion = "list"; 
+    });
+}
+
+app.MapGet("/", () => "Workflow Approval API");
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/", () => "Workflow Approval API");
+// Endpoints
 app.MapRequestEndpoints();
-
+app.MapWorkflowEndpoints();
+app.MapAnalyticsEndpoints();
 app.Run();
+
+
+
+;
+
+
+
+
+
